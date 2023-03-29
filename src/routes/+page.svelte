9556c52e46e62fdbd6e0997724e2cf7bf115c3c6
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-03-29 02:23:00 +0200

    finish merge really

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,26 @@
 <script lang="ts">
-	import Collapsible from './Collapsible.svelte';
-	import CollapsibleCode from './Collapsible.svelte?raw';
+	import CollapsibleDemo from './CollapsibleDemo.svelte';
+	import CollapsibleDemoCode from './CollapsibleDemo.svelte?raw';
 
 	export let open = false;
 	export let disabled = false;
 </script>
 
-<main>
-	<article class="max-w-screen-md w-full">
-		<div class="max-w-80">
-			<div class="flex flex-col items-start gap-2">
-				<p>
-					Outer open: {open}
-				</p>
-				<button
-					class="bg-white text-violet-500 rounded-md px-2 py-1"
-					on:click={() => (open = !open)}
-				>
-					Outer open toggle
-				</button>
-
-				<p>
-					Outer disabled: {disabled}
-				</p>
-				<button
-					class="bg-white text-violet-500 rounded-md px-2 py-1"
-					on:click={() => (disabled = !disabled)}
-				>
-					Outer disabled toggle
-				</button>
-			</div>
-
-			<hr class="my-2" />
+<div class="wrapper">
+	<div class="comp-preview">
+		<h2 class="title">Collapsible</h2>
+		<div class="content">
+			<CollapsibleDemo bind:open bind:disabled />
+			<details>
+				<summary> Tailwind </summary>
+				<pre class="overflow-auto">{CollapsibleDemoCode}</pre>
+			</details>
 		</div>
-
-		<Collapsible bind:open bind:disabled />
-		<details>
-			<summary> Tailwind </summary>
-			<pre class="overflow-auto">{CollapsibleCode}</pre>
-		</details>
-	</article>
-</main>
+	</div>
+</div>
 
 <style lang="postcss">
-	main {
-		background-image: linear-gradient(
-			330deg,
-			theme('colors.purple.500') 0%,
-			theme('colors.indigo.800') 100%
-		);
-
+	.wrapper {
 		display: grid;
 		padding: theme('spacing.4');
 
