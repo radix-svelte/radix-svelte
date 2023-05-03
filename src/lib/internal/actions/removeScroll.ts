c40b8e78e0c4e24065ee1dd617168c83f6ed2d5a
Abiee <hexcy@pm.me>
2023-05-03 19:54:27 +0800

    feat: When opening the Dialog, preserves the scrollbar "gap"

--- src/lib/internal/actions/removeScroll.ts
+++ src/lib/internal/actions/removeScroll.ts
@@ -4,9 +4,15 @@ type Params = {
 	disable?: boolean;
 };
 
+export const getScrollbarWidth = () => {
+	return window.innerWidth - document.documentElement.clientWidth;
+};
+
 export const removeScroll = ((node, params) => {
 	const update = (params: Params) => {
+		const scrollbarWidth = getScrollbarWidth();
 		document.body.style.overflow = params.disable ? 'initial' : 'hidden';
+		document.body.style.paddingRight = params.disable ? '' : scrollbarWidth + 'px';
 	};
 
 	update(params);
