215c44689db049382215b09fca37ad33f62f4d76
Lev Chelyadinov <leva181777@gmail.com>
2023-07-23 15:23:51 +0200

    Upgrade every dependency apart from Svelte

--- src/test-helpers/axeTester.ts
+++ src/test-helpers/axeTester.ts
@@ -4,10 +4,6 @@ import type { Page } from '@playwright/test';
 type ViolationsResult = Awaited<ReturnType<AxeBuilder['analyze']>>['violations'];
 
 export async function axeViolations(page: Page): Promise<ViolationsResult> {
-	// @ts-expect-error Due to a bug in Playwright, we need to use the default export
-	// https://github.com/dequelabs/axe-core-npm/issues/601
-	const accessibilityScanResults = await new AxeBuilder.default({ page })
-		.include('#root')
-		.analyze();
+	const accessibilityScanResults = await new AxeBuilder({ page }).include('#root').analyze();
 	return accessibilityScanResults.violations;
 }
